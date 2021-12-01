46ea1f3ce5bd30cdb1078fe5e1e8ef8ac3e899f1
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-12-01 16:37:59 +0530

    automatic input fetching

--- day.sh
+++ day.sh
@@ -3,7 +3,8 @@ if [[ -f "$1.py" || -z "$1" ]]; then
     exit
 fi
 
-touch $1.in
+curl --silent --cookie "session=$(cat .session)" "https://adventofcode.com/2021/day/1/input" > $1.in
 cp template/template.py $1.py
 
 code $1.py $1.in
+# python3 $1.py
