1dfb25953c094b0ec1acd121d185fb3242aa501a
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-12-12 19:20:07 +0530

    day 4 part 2

--- 4.py
+++ 4.py
@@ -2,6 +2,25 @@ import sys
 import pprint as pprint
 
 def getInput(filepath = None):
+#         return """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+# 22 13 17 11  0
+#  8  2 23  4 24
+# 21  9 14 16  7
+#  6 10  3 18  5
+#  1 12 20 15 19
+
+#  3 15  0  2 22
+#  9 18 13 17  5
+# 19  8  7 25 23
+# 20 11 10 24  4
+# 14 21 16 12  6
+
+# 14 21 17 24  4
+# 10 16 15  9 19
+# 18  8 23 26 20
+# 22 11 13  6  5
+#  2  0 12  3  7"""
         # change the extension of the file from .py to .in | split, slice (excluding the last), join + ".in"
         filepath = filepath  or ".".join(sys.argv[0].split('.')[0:-1]) + ".in"
         with open(filepath) as inFile:
@@ -86,11 +105,16 @@ class BingoBoard():
 
 BOARDS = [BingoBoard(board) for board in BOARDS]
 
-for number in ORDER:
-    for bNo, board in enumerate(BOARDS, 0):
+
+for bingoNumber in ORDER:
+    bNo = 0
+    while bNo < len(BOARDS):
+        board = BOARDS[bNo]
         # pprint.pprint({bNo, board})
-        hasWon = board.checkForMarking(number);
+        hasWon = board.checkForMarking(bingoNumber);
         if hasWon:
-            score = board.getScore(number);
-            pprint.pprint(f"{bNo=} {score=}")
-            exit()
+            score = board.getScore(bingoNumber);
+            pprint.pprint(f"{bNo=} {score=} {bingoNumber=} ")
+            BOARDS.pop(bNo);
+        else:
+            bNo += 1
